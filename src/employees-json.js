8494b9925bf3da22be4263d2da65ae178b6d4d08
Alexander Ponomarenko <aaponomarenko@vtb.ru>
2021-11-07 23:35:16 +0300

    unit1 - homework

---
+++ src/employees-json.js
@@ -0,0 +1,28 @@
+const DATA = {
+    employees: [
+        {
+            id: 1,
+            name: 'Пафнутий',
+            surname: 'Пафнутьев',
+            department: 'IT'
+        },
+        {
+            id: 2,
+            name: 'Вася',
+            surname: 'Васильев',
+            department: 'IT'
+        },
+        {
+            id: 3,
+            name: 'Маша',
+            surname: 'Морева',
+            department: 'Sales'
+        },
+        {
+            id: 4,
+            name: 'Коля',
+            surname: 'Николаев',
+            department: 'Sales'
+        }
+    ]
+};
