7a72cd72f49c49d9b539005b7f03faa3a97c38ff
Alexander Ponomarenko <aaponomarenko@vtb.ru>
2021-11-09 19:29:00 +0300

    unit3 - added package.json, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        'prettier'
+    ],
+    "rules": {
+        "semi": ["warn", "always"],
+        'prettier/prettier': 'error',
+    }
+}; 
